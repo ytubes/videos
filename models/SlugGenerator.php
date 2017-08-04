8b42269dee1a6eb82cd1780e61e976286abf8268
ytubes <adx.informer@gmail.com>
2017-08-05 01:22:17 +0700

    Update SlugGenerator.php

--- models/SlugGenerator.php
+++ models/SlugGenerator.php
@@ -5,37 +5,37 @@ use URLify;
 
 trait SlugGenerator
 {
-	/**
-	 * Генерирует slug исходя из title. Также присоединяет численный суффикс, если слаг не уникален.
-	 *
-	 * @param string $title
-	 * @return string
-	 */
-	public function generateSlug($title)
-	{
-		$slug = URLify::filter($title);
+    /**
+     * Генерирует slug исходя из title. Также присоединяет численный суффикс, если слаг не уникален.
+     *
+     * @param string $title
+     * @return string
+     */
+    public function generateSlug($title)
+    {
+        $slug = URLify::filter($title);
 
-		if (!$slug)
-			$slug = 'default-slug';
+        if (!$slug)
+            $slug = 'default-slug';
 
-			// если слаг существует, добавляем к нему суффикс, до тех пор пока не станет уникальным.
-		if ($this->checkExistsSlug($slug)) {
-			for ($suffix = 1; $this->checkExistsSlug($new_slug = $slug . '-' . $suffix); $suffix++ ) {}
-			$slug = $new_slug;
-		}
+            // если слаг существует, добавляем к нему индекс, до тех пор пока не станет уникальным.
+        if ($this->checkExistsSlug($slug)) {
+            for ($index = 1; $this->checkExistsSlug($new_slug = $slug . '-' . $index); $index++ ) {}
+            $slug = $new_slug;
+        }
 
-		$this->slug = $slug;
-	}
-	/**
-	 * Проверяет существует ли слаг.
-	 *
-	 * @param string $slug
-	 * @return bool
-	 */
-	private function checkExistsSlug($slug)
-	{
-		return (boolean) self::find()
-			->where(['slug' => $slug])
-			->exists();
-	}
+        $this->slug = $slug;
+    }
+    /**
+     * Проверяет существует ли слаг у какой либо записи в базе.
+     *
+     * @param string $slug
+     * @return bool
+     */
+    private function checkExistsSlug($slug)
+    {
+        return (boolean) self::find()
+            ->where(['slug' => $slug])
+            ->exists();
+    }
 }
