13a5b89b2815b32392d1ae196aa19f194e54d743
ytubes <adx.informer@gmail.com>
2017-08-05 00:37:28 +0700

    Update CategoryFinder.php

--- models/finders/CategoryFinder.php
+++ models/finders/CategoryFinder.php
@@ -1,8 +1,6 @@
 <?php
 namespace ytubes\videos\models\finders;
 
-use yii\base\Model;
-
 use ytubes\videos\models\Video;
 use ytubes\videos\models\Category;
 use ytubes\videos\models\RotationStats;
@@ -10,156 +8,72 @@ use ytubes\videos\models\RotationStats;
 /**
  * CategoryFinder представляет собой импровизированное репо для категорий.
  */
-class CategoryFinder extends Model
+class CategoryFinder
 {
-	protected $categoriesIndexedById;
-	protected $categoriesIndexedBySlug;
-	protected $categoriesIdsNamesArray;
+    protected $categoriesIndexedById;
+    protected $categoriesIndexedBySlug;
+    protected $categoriesIdsNamesArray;
 
-	public function __construct($config = [])
-	{
-		parent::__construct($config);
-	}
-
-    /**
-     * @inheritdoc
-     */
-    public function rules()
+    public static function find()
     {
-        return [];
+        return Category::find();
     }
 
-    /**
-     * @inheritdoc
-     */
-    public function scenarios()
+    /*Используется*/
+    public function findBy($where, $order = [])
     {
-        // bypass scenarios() implementation in the parent class
-        return Model::scenarios();
+        return self::find()
+            ->where($where)
+            ->orderBy($order)
+            ->asArray()
+            ->all();
+    }
+    /**
+     * Возвращает все активные категории
+     *
+     * @param string|array $order сортировка категорий.
+     * @return array
+     */
+    public static function getActiveCategories($order = [])
+    {
+        return self::find()
+            ->select([
+                'category_id',
+                'slug',
+                'image',
+                'meta_title',
+                'meta_description',
+                'title',
+                'h1',
+                'description',
+                'seotext',
+                'param1',
+                'param2',
+                'param3',
+                'videos_num',
+                'on_index',
+            ])
+            //->where(['status' => 10])
+            ->orderBy($order)
+            ->asArray()
+            ->all();
     }
 
-	public static function find()
-	{
-		return Category::find();
-	}
+    /*Используется*/
+    public static function findById($id)
+    {
+        return self::find()
+            ->where(['category_id' => $id/*, 'status' => 10*/])
+            ->asArray()
+            ->one();
+    }
 
-	/*Используется*/
-	public function findBy($where, $order = [])
-	{
-		return self::find()
-			->where($where)
-			->orderBy($order)
-			->asArray()
-			->all();
-	}
-	/**
-	 * Возвращает все активные категории
-	 * @param string|array $order сортировка категорий.
-	 * @return array
-	 */
-	public static function getActiveCategories($order = [])
-	{
-		return self::find()
-			->select([
-				'category_id',
-				'slug',
-				'image',
-				'meta_title',
-				'meta_description',
-				'title',
-				'h1',
-				'description',
-				'seotext',
-				'param1',
-				'param2',
-				'param3',
-				'videos_num',
-				'on_index',
-			])
-			//->where(['status' => 10])
-			->orderBy($order)
-			->asArray()
-			->all();
-	}
-
-	/*Используется*/
-	public static function findById($id)
-	{
-		return self::find()
-			->where(['category_id' => $id/*, 'status' => 10*/])
-			->asArray()
-			->one();
-	}
-
-	/*Используется*/
-	public static function findBySlug($slug)
-	{
-		return self::find()
-			->where(['slug' => $slug/*, 'status' => 10*/])
-			->asArray()
-			->one();
-	}
-
-	/*public function getCategoriesIndexedById()
-	{
-		if (null === $this->categoriesIndexedById) {
-			$this->categoriesIndexedById = self::find()
-				->indexBy('category_id')
-				->all();
-		}
-
-		return $this->categoriesIndexedById;
-	}
-
-	public function getCategoryBySlug($slug)
-	{
-		$this->getCategoriesIndexedBySlug();
-
-		if (isset($this->categoriesIndexedBySlug[$slug])) {
-			return $this->categoriesIndexedBySlug[$slug];
-		}
-
-		return null;
-	}
-
-	public function getCategoriesIndexedBySlug()
-	{
-		if (null === $this->categoriesIndexedBySlug) {
-			$this->categoriesIndexedBySlug = self::find()
-				->indexBy('slug')
-				->all();
-		}
-
-		return $this->categoriesIndexedBySlug;
-	}
-
-	public function getCategoryNameById($id)
-	{
-		$this->getCategoriesIdsNamesArray();
-
-		if (isset($this->categoriesIdsNamesArray[$id])) {
-			return $this->categoriesIdsNamesArray[$id];
-		}
-
-		return null;
-	}
-
-	public function getCategoriesIdsNamesArray()
-	{
-		if (null === $this->categoriesIdsNamesArray) {
-			$categories = self::find()
-				->select(['category_id', 'title'])
-				->indexBy('category_id')
-				->asArray()
-				->all();
-
-			if (!empty($categories)) {
-				$this->categoriesIdsNamesArray = array_column($categories, 'title', 'category_id');
-			} else {
-				$this->categoriesIdsNamesArray = [];
-			}
-		}
-
-		return $this->categoriesIdsNamesArray;
-	}*/
+    /*Используется*/
+    public static function findBySlug($slug)
+    {
+        return self::find()
+            ->where(['slug' => $slug/*, 'status' => 10*/])
+            ->asArray()
+            ->one();
+    }
 }
