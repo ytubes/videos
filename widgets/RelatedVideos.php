dd10d77b97311f3fdaccf7479f93b39382479d40
ytubes <adx.informer@gmail.com>
2017-08-07 17:53:21 +0700

    Update RelatedVideos.php

--- widgets/RelatedVideos.php
+++ widgets/RelatedVideos.php
@@ -13,87 +13,102 @@ use ytubes\videos\models\finders\RelatedFinder;
 
 class RelatedVideos extends Widget
 {
-	private $cacheKey = 'videos:widgets:related_videos:';
-	/**
-	 * @var string путь к темплейту виджета
-	 */
-	public $template = __DIR__ . '/views/related_videos.php';
-	/**
-	 * @var integer $video_id
-	 */
-	public $video_id;
-	/**
-	 * @var array Коллекция массивов категорий.
-	 */
-	public $items = [];
+    private $cacheKey = 'videos:widgets:related_videos:';
+    /**
+     * @var string путь к темплейту виджета
+     */
+    public $template = __DIR__ . '/views/related_videos.php';
+    /**
+     * @var integer $video_id
+     */
+    public $video_id;
+    /**
+     * @var array Коллекция массивов категорий.
+     */
+    public $items;
+    /**
+     * @var array диапазон показа релейтедов
+     * Пример: 'range' => [1, 5],
+     */
+    public $range;
 
-	public $enable;
+    public $enable;
 
-	/**
-	 * Initializes the widget
-	 */
-	public function init() {
-		parent::init();
+    /**
+     * Initializes the widget
+     */
+    public function init() {
+        parent::init();
 
-		if (empty($this->video_id)) {
-			throw new InvalidConfigException('Виджет требует определенного video_id');
+        if (empty($this->video_id)) {
+            throw new InvalidConfigException('Виджет требует определенного video_id');
 
-			return;
-		}
-		if (is_null($this->enable)) {
-			$this->enable = Module::getInstance()->settings->get('related_enable', true);
-		}
-	}
+            return;
+        }
+        if (is_null($this->enable)) {
+            $this->enable = Module::getInstance()->settings->get('related_enable', true);
+        }
+    }
 
-	/**
-	 * Runs the widget
-	 *
-	 * @return string|void
-	 */
-	public function run() {
+    /**
+     * Runs the widget
+     *
+     * @return string|void
+     */
+    public function run() {
 
-		if (!$this->enable) {
-			return;
-		}
+        if (!$this->enable) {
+            return;
+        }
 
-		$videos = $this->getItems();
+        $videos = $this->getItems();
 
-		if (empty($videos)) {
-			return;
-		}
+        if (empty($videos)) {
+            return;
+        }
 
-		return $this->renderFile($this->template, [
-        	'data' => [
-        		'videos' => $videos,
+        if (is_array($this->range)) {
+            $rangeStart = ($this->range[0] > 0) ? $this->range[0] - 1 : 0 ;
+            $rangeEnd = (!isset($this->range[1])) ? 1 : $this->range[1] ;
+            $videos = array_slice($videos, $rangeStart, $rangeEnd);
+        }
+
+        return $this->renderFile($this->template, [
+            'data' => [
+                'videos' => $videos,
             ],
-		]);
-	}
+        ]);
+    }
 
-	/**
-	 * Получает "похожие" видео.
-	 *
-	 * @return array
-	 */
+    /**
+     * Получает "похожие" видео.
+     *
+     * @return array
+     */
     private function getItems()
     {
-    	$cacheKey = $this->buildCacheKey();
+        if (!is_null($this->items)) {
+            return $this->items;
+        }
 
-    	$items = Yii::$app->cache->get($cacheKey);
+        $cacheKey = $this->buildCacheKey();
 
-    	if (false === $items) {
-    		$relatedFinder = new RelatedFinder;
-    		$items = $relatedFinder->getVideos($this->video_id);
+        $this->items = Yii::$app->cache->get($cacheKey);
 
-    		if (!empty($items)) {
-    			Yii::$app->cache->set($cacheKey, $items, 300);
-    		}
-    	}
+        if (false === $this->items) {
+            $relatedFinder = new RelatedFinder;
+            $this->items = $relatedFinder->getVideos($this->video_id);
 
-    	return $items;
+            if (!empty($this->items)) {
+                Yii::$app->cache->set($cacheKey, $this->items, 300);
+            }
+        }
+
+        return $this->items;
     }
 
     private function buildCacheKey()
     {
-    	return $this->cacheKey . $this->video_id;
+        return $this->cacheKey . $this->video_id;
     }
 }
